87255a9445b7485d18762784ed8391035133cda3
VABIII <tbengtson82@gmail.com>
2021-12-01 12:26:35 -0600

    Built useDarkMode and useLocalStorage hooks

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,59 @@
+import {useState} from "react";
+
+
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode]  = useState(false);
+
+    return [darkMode, setDarkMode]
+
+};
+
+export default useDarkMode;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
